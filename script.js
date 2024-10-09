8015c1e4c966fe3ce780393db4d1c433664b43f3
jshep23prog <jshep23@gmail.com>
2024-10-09 16:11:45 -0500

    rendering data dynamically fullstack baby!

---
+++ script.js
@@ -0,0 +1,92 @@
+const initialFacts = [
+  {
+    id: 1,
+    text: "React is being developed by Meta (formerly facebook)",
+    source: "https://opensource.fb.com/",
+    category: "technology",
+    votesInteresting: 24,
+    votesMindblowing: 9,
+    votesFalse: 4,
+    createdIn: 2021,
+  },
+  {
+    id: 2,
+    text: "Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%",
+    source:
+      "https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids",
+    category: "society",
+    votesInteresting: 11,
+    votesMindblowing: 2,
+    votesFalse: 0,
+    createdIn: 2019,
+  },
+  {
+    id: 3,
+    text: "Lisbon is the capital of Portugal",
+    source: "https://en.wikipedia.org/wiki/Lisbon",
+    category: "society",
+    votesInteresting: 8,
+    votesMindblowing: 3,
+    votesFalse: 1,
+    createdIn: 2015,
+  },
+];
+//selecting DOM elements
+const btn = document.querySelector(".btn-open");
+const form = document.querySelector('.fact-form')
+const factsList = document.querySelector('.facts-list')
+
+//Create Dom elements: Render facts in list
+factsList.innerHTML = "";
+
+loadFacts();
+//load data from Supabase
+async function loadFacts() {
+  const res = await fetch('https://srolirbulcwloryayndk.supabase.co/rest/v1/facts',
+    {
+      headers: {
+        apikey:
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNyb2xpcmJ1bGN3bG9yeWF5bmRrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjg0OTk4NTQsImV4cCI6MjA0NDA3NTg1NH0.arcnlRLHQPNdJWiijg_YIoeq4ehhnoVoBxEn8Zoqh-E"
+        ,
+        authorization:
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNyb2xpcmJ1bGN3bG9yeWF5bmRrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjg0OTk4NTQsImV4cCI6MjA0NDA3NTg1NH0.arcnlRLHQPNdJWiijg_YIoeq4ehhnoVoBxEn8Zoqh-E"
+      },
+    }
+  );
+  const data = await res.json();
+  console.log(data)
+  createFactsList(data)
+}
+
+
+// createFactsList(res);
+
+function createFactsList(dataArray) {
+  const htmlArr = dataArray.map(
+    (fact) => `<li class="fact">
+    <p>
+    ${fact.text}
+    <a
+    class="source"
+    href="${fact.source}"
+    target="_blank"
+    >(Source)</a>
+    </p>
+    <span class="tag" style="background-color:#3b82f6">${fact.category}</span>
+    </li>`
+  );
+  const html = htmlArr.join("");
+  factsList.insertAdjacentHTML("afterbegin", html)
+}
+
+//add event listener for Share a fact button toggle form visibility
+btn.addEventListener('click', function () {
+  if (form.classList.contains('hidden')) {
+    form.classList.remove('hidden');
+    btn.textContent = 'Close'
+  }
+  else {
+    form.classList.add('hidden');
+    btn.textContent = 'Share a Fact'
+  }
+})
